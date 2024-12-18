896c3cdcd9436572c49ea68d68ce23e7cfc46529
Surve <surve8729@gmail.com>
2024-12-19 00:47:04 +0530

    Add user attended events on their dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @created_events = @user.created_events
+    @attended_events = @user.attended_events
   end
 end
