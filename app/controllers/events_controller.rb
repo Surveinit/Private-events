3aecc446a4e15147aa2450d2fa4ffd47423f19fc
Surve <surve8729@gmail.com>
2024-12-18 22:00:52 +0530

    Create Events show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,7 @@ class EventsController < ApplicationController
   def show
     @user= current_user
     @created_events = @user.created_events
+    @event = Event.find(params[:id])
   end
 
   def new
